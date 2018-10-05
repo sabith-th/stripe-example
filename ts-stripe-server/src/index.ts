71a00c34b0d76dbee57c4bface27344271080996
SabithHaneef <sabith.th.07@gmail.com>
2018-10-05 23:18:01 +0530

    remove submodule

---
+++ ts-stripe-server/src/index.ts
@@ -0,0 +1,37 @@
+import { ApolloServer } from "apollo-server-express";
+import * as express from "express";
+import * as session from "express-session";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }: any) => ({ req })
+  });
+
+  await createConnection();
+
+  const app = express();
+  app.use(
+    session({
+      secret: "thetragedyofdarthplagueis",
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+
+  server.applyMiddleware({ app, cors: {
+    credentials: true,
+    origin: "http://localhost:3000"
+  } });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
