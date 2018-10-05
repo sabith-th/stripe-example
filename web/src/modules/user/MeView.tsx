bb3b664d603d2d21164756bbdaba5d19eb746c86
SabithHaneef <sabith.th.07@gmail.com>
2018-10-05 22:06:26 +0530

    setup server with user register and login with sessions and setup front end with React

---
+++ web/src/modules/user/MeView.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export class MeView extends React.PureComponent {
+  /**
+   * render
+   */
+  public render() {
+    return <div>Me View</div>;
+  }
+}
---
+++ web/src/modules/user/RegisterView.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export class RegisterView extends React.PureComponent {
+  /**
+   * render
+   */
+  public render() {
+    return <div>Register View</div>;
+  }
+}
