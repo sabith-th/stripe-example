32f9edbca90299cfb2582b0726644c8d8ce7d000
SabithHaneef <sabith.th.07@gmail.com>
2018-10-06 15:31:25 +0530

    create Subscription route and add stripe subscription and send token to server

---
+++ web/src/modules/account/SubscribeUser.tsx
@@ -0,0 +1,36 @@
+import { gql } from "apollo-boost";
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import StripeCheckout from "react-stripe-checkout";
+import {
+  CreateSubscriptionMutation,
+  CreateSubscriptionMutationVariables
+} from "../../schemaTypes";
+
+const createSubscriptionMutation = gql`
+  mutation CreateSubscriptionMutation($source: String!) {
+    createSubscription(source: $source) {
+      id
+      email
+    }
+  }
+`;
+export default class SubscribeUser extends React.PureComponent {
+  public render() {
+    return (
+      <Mutation<CreateSubscriptionMutation, CreateSubscriptionMutationVariables>
+        mutation={createSubscriptionMutation}
+      >
+        {mutate => (
+          <StripeCheckout
+            // tslint:disable-next-line:jsx-no-lambda
+            token={async token => {
+              mutate({ variables: { source: token.id } });
+            }}
+            stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY!}
+          />
+        )}
+      </Mutation>
+    );
+  }
+}
